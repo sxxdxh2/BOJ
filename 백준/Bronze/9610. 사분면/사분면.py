a1d17744b87828c4cccdf59e5982a031a0485134
DaHee <63050400+sxxdxh2@users.noreply.github.com>
2025-10-07 17:12:00 +0900

    [Bronze III] Title: 사분면, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9610. 사분면/사분면.py
@@ -0,0 +1,23 @@
+n = int(input())
+q1 = q2 = q3 = q4 = axis = 0
+
+for _ in range(n):
+
+    a,b = map(int, input().split())
+
+    if a>0 and b>0:
+        q1 += 1
+    elif a<0 and b>0:
+        q2 +=1
+    elif a<0 and b<0:
+        q3 +=1
+    elif a>0 and b<0:
+        q4 +=1
+    else:
+        axis +=1
+
+print(f"Q1: {q1}")
+print(f"Q2: {q2}")
+print(f"Q3: {q3}")
+print(f"Q4: {q4}")
+print(f"AXIS: {axis}")
