e1dfd045ac9a50eef312fb72503a6667b10b7e57
DaHee <63050400+sxxdxh2@users.noreply.github.com>
2025-10-07 19:18:23 +0900

    [Bronze III] Title: 전자레인지, Time: 44 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10162. 전자레인지/전자레인지.py
+++ 백준/Bronze/10162. 전자레인지/전자레인지.py
@@ -1,35 +1,14 @@
-a = 300
-b = 60
-c = 10
-
 t = int(input())
 
-if t >= a:
-    x = t // a
-    if (t - a * x) >= b:
-        y = (t - a * x) // b
-    else:
-        y = t - a * x
-
-    if (t - a * x - b * y) >= c:
-        z = (t - a * x - b * y) // c
-    else:
-        z = t - a * x - b * y
-
-elif b <= t < a:
-    x = 0
-    y = t // b
-    if (t - b * x) >= c:
-        z = (t - b * y) // c
-    else:
-        z = t - b * y
-
-elif t < b:
-    x = 0
-    y = 0
-    z = t // c
-
-if (x * a + y * b + z * c) == t:
-    print(f"{x} {y} {z}")
-else:
+if t % 10 != 0:
     print(-1)
+else:
+    a = t // 300
+    t %= 300
+
+    b = t // 60
+    t %= 60
+
+    c = t // 10
+
+    print(a, b, c)
