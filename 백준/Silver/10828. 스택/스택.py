129ae2782616cf39d52b816371bff32aeb1a258d
DaHee <63050400+sxxdxh2@users.noreply.github.com>
2025-11-07 13:44:45 +0900

    [Silver IV] Title: 스택, Time: 496 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,29 @@
+N = int(input())
+
+stack = []
+
+for _ in range(N):
+    cmd = input().split()
+
+    if cmd[0] == "push":
+        stack.append(cmd[1])
+
+    elif cmd[0] == "pop":
+        if not stack:
+            print(-1)
+        else:
+            # stack.pop(cmd[1]) -> XXX
+            # pop()은 인덱스가 아니라 맨 위 제거
+            print(stack.pop())  
+
+    elif cmd[0] == "size":
+        print(len(stack))
+
+    elif cmd[0] == "empty":
+        print(1 if not stack else 0)
+
+    elif cmd[0] == "top":
+        if not stack:
+            print(-1)
+        else:
+            print(stack[-1])
