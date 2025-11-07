5ff9472b3901d6de21f0f335777905ff8cf5e5c7
DaHee <63050400+sxxdxh2@users.noreply.github.com>
2025-11-07 13:52:29 +0900

    [Silver IV] Title: 스택, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,8 +1,10 @@
-N = int(input())
+import sys
+
+input = sys.stdin.readline  # 입출력 속도 개선
 
 stack = []
 
-for _ in range(N):
+for _ in range(int(input())):  # N 입력만큼 반복
     cmd = input().split()
 
     if cmd[0] == "push":
@@ -14,13 +16,16 @@ for _ in range(N):
         else:
             # stack.pop(cmd[1]) -> XXX
             # pop()은 인덱스가 아니라 맨 위 제거
-            print(stack.pop())  
+            print(stack.pop())
 
     elif cmd[0] == "size":
         print(len(stack))
 
     elif cmd[0] == "empty":
-        print(1 if not stack else 0)
+        if not stack:
+            print(1)
+        else:
+            print(0)
 
     elif cmd[0] == "top":
         if not stack:
