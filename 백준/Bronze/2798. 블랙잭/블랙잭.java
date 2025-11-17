a8cdab06fe075d7a8cbb49fc91c3c429d9fe563a
DaHee <63050400+sxxdxh2@users.noreply.github.com>
2025-11-17 10:58:11 +0900

    [Bronze II] Title: 블랙잭, Time: 172 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,33 @@
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int maxSum = 0;
+
+        for (int i = 0; i < N - 2; i++) {
+            for (int j = i + 1; j < N - 1; j++) {
+                for (int k = j + 1; k < N; k++) {
+                    int sum = arr[i] + arr[j] + arr[k];
+                    if (sum <= M && sum > maxSum) {
+                        maxSum = sum;
+                    }
+                }
+            }
+        }
+        System.out.println(maxSum);
+
+    }
+}
