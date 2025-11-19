112e2fb6f56be5ddddfecf8d66b8f1ab81bcd6c6
DaHee <63050400+sxxdxh2@users.noreply.github.com>
2025-11-19 09:34:44 +0900

    [Silver II] Title: 로또, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.py
+++ 백준/Silver/6603. 로또/로또.py
@@ -1,20 +1,12 @@
-def DFS(L, start):
+import itertools as it
 
-    if L == 6:
-        print(*arr)
-        return
-    else:
-        for i in range(start, k):
-            arr[L] = s[i]
-            DFS(L + 1, i + 1)
+while True:
+    k, *s = map(int, input().split())
+    arr = [0] * 6
+    if k == 0:
+        break
 
+    for x in it.combinations(s, 6):
+        print(*x)
 
-if __name__ == "__main__":
-
-    while True:
-        k, *s = map(int, input().split())  # 언패킹
-        arr = [0] * 6
-        if k == 0:
-            break
-        DFS(0, 0)
-        print()
+    print()
